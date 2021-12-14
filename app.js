5f4380709ef92c69dce42d797c757f583477b640
Shuheng Luo <robinlsh0809@gmail.com>
2021-12-15 00:21:32 +0800

    login part finished

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-// app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
+    const app = this
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+      success (res) {
+        console.log(res.code)
+        if (res.code) {
+          wx.request({
+            url: `${app.globalData.baseUrl}/login`,
+            method: 'POST',
+            data: {
+              code: res.code
+            },
+            success(res){
+              console.log(res.data)
+              wx.setStorageSync('user', res.data.user)
+              wx.setStorageSync('headers', res.data.headers)
+            }
+          
+          })
+        } else {
+          console.log('Error' + res.errMsg)
+        }
+      },
+      fail: function () {
+        callback(false)
+      },
     })
   },
   globalData: {
-    models: [
-      {
-      "id": 1,
-      "title": "two year old Golden retriver",
-      "name": "momo",
-      "description": "many-many year of professional modeling",
-      "photo": "https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00518-1011.jpg"
-      },
-
-      {
-      "id" :2,
-      "title":"three years old another dog",
-      "name":"kim",
-      "description":"kim has so many year expirience with pet modeling"            
-      },
-
-      {
-      "id" :3,
-      title:"four years old french bulldog",
-      name:"Leo",
-      description:"kim has so many year expirience with pet modeling"            
-      }
-  ]    
+    baseUrl: 'http://localhost:3000/api/v1',
+    // baseUrl: 'your production url'
   }
 })
--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -1,71 +1,30 @@
-// pages/homepage/homepage.js
+// pages/stories/index.js
+const app = getApp()
+// this means that i am calling the app.js file 
+
 Page({
 
-    /**
-     * Page initial data
-     */
-    data: {
+  data: {
+  },
+  onLoad: function (options) {
 
-    },
+  },
+  onReady: function () {
+  },
 
-    
-
-
-    /**
-     * Lifecycle function--Called when page load
-     */
-    onLoad: function (options) {
-
-    },
-
-
-    /**
-     * Lifecycle function--Called when page is initially rendered
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * Lifecycle function--Called when page show
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * Lifecycle function--Called when page hide
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * Lifecycle function--Called when page unload
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * Page event handler function--Called when user drop down
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * Called when page reach bottom
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * Called when user click on the top right corner to share
-     */
-    onShareAppMessage: function () {
-
+  onShow: function () {
+    const page = this
+    const auth = wx.getStorageSync('auth')
+    const header = {
+      'X-User-Email': auth.email,
+      'X-User-Token': auth.token
     }
+    wx.request({
+      url: `${getApp().globalData.baseUrl}/services`,
+      success(res){
+        console.log(res.data)
+        page.setData(res.data)
+      }
+    })
+  },
 })
-
