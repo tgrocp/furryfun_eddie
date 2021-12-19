a0ba4ecd247a3e394e17659cac2a56d865a5d823
Roxanne Zhao <roxannezhao199@gmail.com>
2021-12-19 13:39:16 +0800

    connected data to myaccount

--- pages/myaccount/myaccount.js
+++ pages/myaccount/myaccount.js
@@ -15,15 +15,11 @@ Page({
      },
 
     onLoad: function (options) {
+       
         const page = this;
-        wx.request({
-          url: `http://localhost:3000/api/v1/users/${parseInt(options.id)}`,
-          method: 'GET',
-          sueecss(res) {
-              console.log (res.data.user)
-              page.setData({ user: res.data.user })
-            }
-        })  
+        page.setData({ user: getApp().globalData.user })
+        // console.log(page.data)
+       
     },
 
     onReady: function () {
