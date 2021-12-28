57bdfe66dd83793a57c080459fac62d2b39e623c
zhenpengxu <tgrocp@gmail.com>
2021-12-28 22:05:22 +0800

    createWXML

--- pages/homepage/create-page.js
+++ pages/homepage/create-page.js
@@ -19,13 +19,30 @@ Page({
             }
         });
       },
+      radioChange: function(e) {
+       let gender=e.detail.value
+       console.log(gender)
+      },
       
+      bindPickerChange: function(e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+         index: e.detail.value
+        })
+        },
     /**
      * 页面的初始数据
      */
-    data: {
 
+
+
+
+    data: {
+      imgs: [],//本地图片地址数组
+      picPaths:[],//网络路径
     },
+
+
 
     /**
      * 生命周期函数--监听页面加载
@@ -33,6 +50,60 @@ Page({
     onLoad: function (options) {
 
     },
+
+//选择图片tap
+  chooseImageTap: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择'],
+      itemColor: "#00000",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+  },
+//选择图片
+  chooseWxImage: function (type) {
+    var that = this;
+    var imgsPaths = that.data.imgs;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res.tempFilePaths[0]);
+        that.upImgs(res.tempFilePaths[0], 0) //调用上传方法
+      }
+    }) 
+  },
+
+  //上传服务器
+  upImgs: function (imgurl, index) {
+    var that = this;
+    wx.uploadFile({
+      url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxx',//
+      filePath: imgurl,
+      name: 'file',
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+      formData: null,
+      success: function (res) {
+        console.log(res) //接口返回网络路径
+        var data = JSON.parse(res.data)
+          that.data.picPaths.push(data['msg'])
+          that.setData({
+            picPaths: that.data.picPaths
+          })
+          console.log(that.data.picPaths)
+      }
+    })
+  },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
