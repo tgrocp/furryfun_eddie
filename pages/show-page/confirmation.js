23fd6ab823e43f1d8905abc15f80227795123feb
Cheng Chen <blue200912@gmail.com>
2021-12-28 22:05:34 +0800

    showpage_popup

---
+++ pages/show-page/confirmation.js
@@ -0,0 +1,72 @@
+// pages/show-page/confirmation.js
+Page({
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  missingInfo: function ( ) {
+    wx.showToast({
+      title: 'please fill the form',
+      icon: "none",
+      duration:2000
+    })
+  },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
