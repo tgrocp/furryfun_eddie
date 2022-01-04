417af1f6ff78403bca0e438ca1e584288f5b1dee
Roxanne Zhao <roxannezhao199@gmail.com>
2022-01-04 22:08:40 +0800

    added filter feature

--- pages/category-modelling/category-modelling.js
+++ pages/category-modelling/category-modelling.js
@@ -5,7 +5,28 @@ Page({
         const id = e.currentTarget.dataset.id
         wx.navigateTo({
         url: `/pages/show-page/show-page?id=${id}`,
+    
         })
+    },
+
+    filterAnimal: function (e) {
+        // console.log(e.mark.animal)
+        const animal = e.mark.animal
+        // console.log(animal)
+        const pets = this.data.pets
+        if (animal===this.data.animalMark) {
+           this.setData({ isFilter:false }) 
+           return
+        }
+        let animalList 
+        if (animal==="dog"){ 
+            animalList = pets.filter((pet)=> pet.pet_type === "dog") 
+        } else if (animal==="cat") {
+            animalList = pets.filter((pet)=> pet.pet_type === "cat")
+        } else {
+            animalList = pets.filter((pet)=> pet.pet_type === "others")
+        }
+        this.setData({ animalList, isFilter:true, animalMark:animal })
     },
 
     onShow() {
