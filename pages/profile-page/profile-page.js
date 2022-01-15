95c093ff628b0b61da2698ebcd6e01426c08f013
Shuheng Luo <robinlsh0809@gmail.com>
2022-01-15 21:03:33 +0800

    still got the problem of show booking information, need to fix tomorrow

--- pages/profile-page/profile-page.js
+++ pages/profile-page/profile-page.js
@@ -10,16 +10,49 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        currentData : 0,
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        const app = getApp()
-        const globalData = app.globalData
-        this.setData(globalData)
+        const page = this;
+        // let images = getApp().globalData.modelling_services
+        // page.setData({ images: images })
+        wx.getStorage({
+            key: "user",
+            success: (res) => {
+              const name = res.data.user_name
+              const phone = res.data.phone_number
+              const id = res.data.id
+              
+              page.setData({
+                name: name,
+                phone: phone,
+                id: id
+              })
+            }
+        });
+
+        wx.request({
+            url: `${getApp().globalData.baseUrl}/pets`,
+            method: 'GET',
+            success(res) {
+                console.log(res.data)
+                page.setData({pets: res.data.pets})
+              }
+        });  
+        
+        let id = this.data.id;
+        wx.request({
+            url: `${getApp().globalData.baseUrl}/users/${id}/bookings`,
+            method: 'GET',
+            success(res) {
+                console.log(res.data)
+                page.setData({bookings: res.data.bookings})
+              }
+        });  
     },
 
     /**
@@ -29,23 +62,41 @@ Page({
 
     },
 
+    bindchange:function(e){
+        const that  = this;
+        that.setData({
+          currentData: e.detail.current
+        })
+    },
+    //点击切换，滑块index赋值
+    checkCurrent:function(e){
+      const that = this;
+   
+      if (that.data.currentData === e.target.dataset.current){
+          return false;
+      }else{
+   
+        that.setData({
+          currentData: e.target.dataset.current
+        })
+      }
+    },
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-        const page = this;
-        // let images = getApp().globalData.modelling_services
-        // page.setData({ images: images })
-        wx.request({
-          url: `${getApp().globalData.baseUrl}/pets`,
-          method: 'GET',
-          success(res) {
-              console.log(res.data)
-              page.setData({pets: res.data.pets})
-           }
-        })
-
-        
+        // const page = this;
+        // // let images = getApp().globalData.modelling_services
+        // // page.setData({ images: images })
+        // let id = this.data.id;
+        // wx.request({
+        //     url: `${getApp().globalData.baseUrl}/users/${id}/bookings`,
+        //     method: 'GET',
+        //     success(res) {
+        //         console.log(res.data)
+        //         page.setData({bookings: res.data.bookings})
+        //       }
+        // })  
     },
 
     /**
