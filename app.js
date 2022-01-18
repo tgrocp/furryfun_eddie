444cc344ab6045da573b6d10446ba26dded945b2
Roxanne Zhao <roxannezhao199@gmail.com>
2022-01-18 12:36:23 +0800

    landing page css

--- app.js
+++ app.js
@@ -28,21 +28,8 @@ App({
 
   globalData: {
     userInfo: null,
+    // baseUrl: 'https://furry-fun.wogengapp.cn/api/v1',
     baseUrl: 'http://localhost:3000/api/v1',
     // baseUrl: "https://furryfun.herokuapp.com/api/v1",
-    // modelling_services: [
-    //   {
-    //     "image": "/images/Golden-retriever.jpg"
-    //   },
-    //   {
-    //     "image": "/images/Chinese-rural-dog.jpg"
-    //   },
-    //   {
-    //     "image": "/images/British-shorthair.jpg"
-    //   },
-    //   {
-    //     "image": "/images/Alpaca.jpg"
-    //   }
-    // ]
   }
 })
